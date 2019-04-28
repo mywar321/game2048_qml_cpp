a2245a5007c29fd8ba117ff392bde5d8226408ae
leess <mywar321@gmail.com>
2019-04-28 14:22:06 +0800

    add gamecontroller.cpp

---
+++ src/gamecontroller.cpp
@@ -0,0 +1,64 @@
+﻿#include "gamecontroller.h"
+#include "tilemodel.h"
+#include <QDebug>
+GameController::GameController(QObject *parent) : QObject(parent)
+{
+    for (int i=0;i<16;i++) {
+        m_tiles.append(new TileModel(i/4,i%4,i+1));
+    }
+}
+
+QQmlListProperty<TileModel> GameController::tileList()
+{
+    return QQmlListProperty<TileModel>(this,nullptr,
+                                       GameController::append_tile,
+                                       GameController::count_tile,
+                                       GameController::at_tile,
+                                       GameController::clear_tile);
+}
+
+void GameController::moveLeft()
+{
+    m_tiles.at(0)->setValue(9);
+
+    emit generateNew(3);
+}
+
+void GameController::generateNewTile()
+{
+    m_tiles.at(3)->setValue(0);
+
+    emit generateNew(3);
+}
+
+void GameController::append_tile(QQmlListProperty<TileModel> *list, TileModel *tile)
+{
+    GameController *controller = qobject_cast<GameController *>(list->object);
+    if (tile != nullptr)
+        controller->m_tiles.append(tile);
+}
+
+int GameController::count_tile(QQmlListProperty<TileModel> *list)
+{
+    GameController *controller = qobject_cast<GameController *>(list->object);
+    return  controller->m_tiles.count();
+}
+
+TileModel* GameController::at_tile(QQmlListProperty<TileModel> *list, int i)
+{
+    GameController *controller = qobject_cast<GameController *>(list->object);
+    return controller->m_tiles.at(i);
+}
+
+void GameController::clear_tile(QQmlListProperty<TileModel> *list)
+{
+    GameController *controller = qobject_cast<GameController *>(list->object);
+    controller->m_tiles.clear();
+}
+
+
+
+
+
+
+
---
+++ src/gamecontroller.h
@@ -0,0 +1,30 @@
+﻿#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+
+#include <QObject>
+#include <QQmlListProperty>
+class TileModel;
+class GameController : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<TileModel> tileList READ tileList NOTIFY tileListChanged)
+public:
+    explicit GameController(QObject *parent = nullptr);
+
+    QQmlListProperty<TileModel> tileList();
+    Q_INVOKABLE void moveLeft();
+    Q_INVOKABLE void generateNewTile();
+signals:
+    void tileListChanged();
+    void generateNew(int i);
+public slots:
+
+private:
+    static void append_tile(QQmlListProperty<TileModel> *list, TileModel *tile);
+    static int count_tile(QQmlListProperty<TileModel> *list);
+    static TileModel* at_tile(QQmlListProperty<TileModel> *list, int i);
+    static void clear_tile(QQmlListProperty<TileModel> *list);
+    QList<TileModel *> m_tiles;
+};
+
+#endif // GAMECONTROLLER_H

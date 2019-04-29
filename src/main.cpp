843acc0578dcbe2ba60ee9ee94897f29f3edc8f0
leess <mywar321@gmail.com>
2019-04-30 03:12:32 +0800

    修改程序默认字体为16号黑体

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 */
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFont>
 #include "tilemodel.h"
 #include "gamecontroller.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
+    QFont font;
+    font.setFamily("Simhei");
+    font.setPixelSize(16);
     QGuiApplication app(argc, argv);
-
+    app.setFont(font);
     //qmlRegisterType<TileModel>("com.game2048.TileModel",1,0,"TileModel");
     qmlRegisterType<GameController>("com.game2048.GameController",1,0,"GameController");
     QQmlApplicationEngine engine;
